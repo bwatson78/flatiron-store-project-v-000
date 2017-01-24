677fd5716845f0242f8b900cfd1084377bf89c27
Brad Watson <brad.watson.orlando@gmail.com>
2017-01-24 13:39:34 -0500

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,36 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items, dependent: :destroy
+  has_many :items, through: :line_items
+
+  def total
+    total = 0
+    self.line_items.each do |line_item|
+      total += line_item.item.price * line_item.quantity
+    end
+    total
+  end
+
+  def add_item(item)
+    line_item = self.line_items.find_by(item_id: item)
+    if line_item
+      line_item.quantity += 1
+    else
+      line_item = self.line_items.build(item_id: item)
+    end
+    line_item
+  end
+
+  def checkout
+    self.status = 'submitted'
+    self.save
+    self.line_items.each do |line_item|
+      line_item.item.inventory -= line_item.quantity
+      line_item.item.save
+    end
+    self.user.current_cart_id = nil
+    self.user.save
+  end
+
 
 end
